e854b52fc605416deed0cf0a8049c41df748b530
JacekSlotwinski <jaceks579@gmail.com>
2024-05-28 00:08:46 +0200

    Add files via upload

--- Character.cpp
+++ Character.cpp
@@ -1,7 +1,7 @@
 #include "include/Character.h"
 #include "include/Dash.h"
-#include "include/Chronobreak.h"
 #include <SFML/Graphics.hpp>
+#include "include/Zap.h"
 
 Character::Character()
     : player_shape(15.0f), hp(100) {
@@ -9,12 +9,14 @@ Character::Character()
     player_shape.setFillColor(sf::Color::Green);
 
     skill_first_slot = new Dash(1000.0f, player_shape);
-    skill_second_slot = new Chronobreak(player_shape, &hp);
+    skill_second_slot = nullptr;
 }
 
 Character::~Character() {
     delete skill_first_slot;
-    delete skill_second_slot;
+    if (skill_second_slot) { // drugi skill nie musi istnieć
+        delete skill_second_slot;
+    }
 }
 
 void Character::move(sf::Vector2f xy_distance) {
@@ -23,4 +25,12 @@ void Character::move(sf::Vector2f xy_distance) {
 
 sf::Vector2f Character::getPosition() {
     return player_shape.getPosition();
+}
+
+void Character::setPosition(sf::Vector2f position) {
+    player_shape.setPosition(position);
+}
+
+void Character::draw(sf::RenderWindow& window) {
+    window.draw(player_shape);
 }

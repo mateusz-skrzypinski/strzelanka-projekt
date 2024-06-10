37556ab16c14d18e6d9eb8cd65120c181e78376e
JacekSlotwinski <jaceks579@gmail.com>
2024-06-10 17:39:13 +0200

    Add files via upload

--- include/Monster.h
+++ include/Monster.h
@@ -2,16 +2,26 @@
 #define MONSTER_H
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 
-class Monster : public sf::RectangleShape {
+class Monster : public sf::Sprite {
 public:
     float hp;
     float speed;
     float stun;
 
+    // animacja
+    std::vector<sf::IntRect> frame_dimentions;
+    static sf::Texture monster_texture;
+    int frame_number;
+    float scale_factor;
+    float last_frame;
+    float dying_time;
+
     Monster();
     void reduce_stun(float dt);
-    void draw(sf::RenderWindow &window, float dt);
+    void draw(sf::RenderWindow &window, float dt, sf::Vector2f character_xy);
+    void dying_animate(sf::RenderWindow &window, float dt);
 };
 
 #endif // MONSTER_H

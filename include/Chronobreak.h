af7df70c28b39e134a97ad11276aeedac97f8fc8
JacekSlotwinski <jaceks579@gmail.com>
2024-06-13 11:47:23 +0200

    Update Chronobreak.h

--- include/Chronobreak.h
+++ include/Chronobreak.h
@@ -9,7 +9,7 @@ public:
     sf::Vector2f xy_to_teleport;
     bool is_teleport_set;
     int hp_before_teleport;
-    float *player_hp;
+    int *player_hp;
 
     sf::Texture teleport_texture;;
     sf::Sprite animation_sprite;
@@ -18,7 +18,7 @@ public:
     bool second_animation;
     float alpha;
 
-    Chronobreak(sf::Sprite& arg_player, sf::RectangleShape &arg_hit_box, float *arg_player_hp);
+    Chronobreak(sf::Sprite& arg_player, sf::RectangleShape &arg_hit_box, int *arg_player_hp);
     void use_skill(sf::Vector2f arg_mouse_position) override;
     void teleport_character();
     void set_xy();
